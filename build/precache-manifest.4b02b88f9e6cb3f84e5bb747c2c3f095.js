de9a665362d630f2b5de1278078a03f1936f9468
iammohammedkhasim <35887969+iammohammedkhasim@users.noreply.github.com>
2020-02-14 11:46:18 +0530

    Add temp Build files

---
+++ build/precache-manifest.4b02b88f9e6cb3f84e5bb747c2c3f095.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c0e28d2fcd08b72567b3866b47ae08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e28376d8c8e945935d7",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "d01681e17a218203b83f",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "0e28376d8c8e945935d7",
+    "url": "/static/js/2.333efc40.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/static/js/2.333efc40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d01681e17a218203b83f",
+    "url": "/static/js/main.0e825a78.chunk.js"
+  },
+  {
+    "revision": "46b8ad6f76949eb705bb",
+    "url": "/static/js/runtime-main.aa8122af.js"
+  }
+]);
